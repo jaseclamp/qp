e31330cf07a14cf1b959dad823afcc5f4c165f4d
jaseclamp <jase.clamp@gmail.com>
2015-04-01 07:07:55 +0000

    edit page

--- Quickpic/Block/Adminhtml/Edit.php
+++ Quickpic/Block/Adminhtml/Edit.php
@@ -2,23 +2,28 @@
 class Refracter_Quickpic_Block_Adminhtml_Edit extends Mage_Adminhtml_Block_Widget
 {
 	
+	var $token; 
+	
 	public function __construct()
     {
-		define('__RemoteURL','http://stageapp-01.madisontech.com.au/quickpic/remote/get');
-		$token = Mage::getStoreConfig('quickpic/remote'); 
-		$token = json_decode($token,true); 
-		$token = $token['token']; 
+		define('__RemoteURL','http://stageapp-01.madisontech.com.au/quickpic/remote');
+		$this->token = Mage::getStoreConfig('quickpic/remote'); 
+		$this->token = json_decode($this->token,true); 
+		$this->token = $this->token['token']; 
     }
     
     public function getImages()
     { 
 			
-		$productId = $this->getVar('id'); 
+		$productId  = (int) $this->getRequest()->getParam('id');
+		
 		$product = Mage::getModel('catalog/product')->load($productId); 
 		
 		$query = $product->getName();
 		
-		$json = file_get_contents( __RemoteURL . '?token='.$token.'query=' . urlencode( $query ) );
+		$_url = __RemoteURL . '/get?token='.$this->token.'&query=' . urlencode( $query );
+		
+		$json = file_get_contents( $_url );
 		
 		$data = json_decode($json,true); 
 	
